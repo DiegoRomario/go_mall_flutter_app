698750bc5a12002a2783fdd40e0fc62212532b11
Diego Romario <diego.romario@outlook.com>
2020-03-01 09:54:22 -0300

    Adding SliverAppBar

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'product.page.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -14,6 +16,42 @@ class HomePage extends StatelessWidget {
               height: 60,
             ),
             search(),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "Categories",
+              style: TextStyle(fontSize: 30),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 90,
+              child: categoryList(),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  "Best Selling",
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+                FlatButton(onPressed: () {}, child: Text("See All"))
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 335,
+              child: productList(context),
+            )
           ],
         ),
       ),
@@ -60,6 +98,124 @@ Widget search() {
             ),
           ),
         )
+      ],
+    ),
+  );
+}
+
+Widget categoryList() {
+  return Container(
+    child: ListView(
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+      ],
+    ),
+  );
+}
+
+Widget categoryItem() {
+  return Container(
+    width: 70,
+    height: 70,
+    margin: EdgeInsets.all(10),
+    padding: EdgeInsets.all(10),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      boxShadow: [
+        new BoxShadow(
+          color: Colors.black12,
+          offset: new Offset(1, 1),
+          blurRadius: 5,
+          spreadRadius: 2,
+        ),
+      ],
+      borderRadius: BorderRadius.all(
+        Radius.circular(64),
+      ),
+    ),
+    child: Image.asset("assets/Icon_Devices.png"),
+  );
+}
+
+Widget productItem(BuildContext context) {
+  return Container(
+    padding: EdgeInsets.all(10),
+    margin: EdgeInsets.all(5),
+    width: 170,
+    color: Colors.black12,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ProductPage(),
+              ),
+            );
+          },
+          child: Image.asset(
+            "assets/product-1.png",
+            width: 170,
+            height: 170,
+            fit: BoxFit.cover,
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          height: 60,
+          child: Text(
+            "Product tittle",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w300,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        Text(
+          "Marca",
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        Text(
+          "\$ 200",
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w900,
+            color: Colors.green,
+          ),
+        )
+      ],
+    ),
+  );
+}
+
+Widget productList(BuildContext context) {
+  return Container(
+    child: ListView(
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        productItem(context),
+        productItem(context),
+        productItem(context),
+        productItem(context),
+        productItem(context),
       ],
     ),
   );
